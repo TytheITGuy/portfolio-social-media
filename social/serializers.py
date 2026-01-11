fa64631512013b5c0aaadf9cd023d119eb4cd187
Tyler Literski <tylerliterski@gmail.com>
2026-01-10 22:43:25 -0500

    Add likes endpoints and post like metadata

---
+++ social/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Post
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source="author.username")
+    
+    class Meta:
+        model = Post
+        fields = ["id", "author", "author_username", "content", "created_at"]
+        read_only_fields = ["id", "author", "author_username", "created_at"]
+        
+        
