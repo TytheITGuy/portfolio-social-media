8a04ef1afc8712d45d969590c0e0fd36d8fba730
Tyler Literski <tylerliterski@gmail.com>
2026-01-12 10:13:56 -0500

    Add pagination to posts feed

--- config/settings.py
+++ config/settings.py
@@ -119,9 +119,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = "/api/"
+LOGOUT_REDIRECT_URL = "/api/"
+
+
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_PAGINATION_CLASS": "social.pagination.StandardResultsSetPagination",
+    "PAGE_SIZE": 10,
 }
+
