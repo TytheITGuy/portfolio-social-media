735ad270057725e3983116bfcf257d4d9e2ab3ae
Tyler Literski <tylerliterski@gmail.com>
2026-05-12 22:45:03 -0400

    Temporarily enable debug for deployment troubleshooting

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g#8zlqs9=_95x2uq92d7vsmdq+@vza$s6rsudtrpc(wvw#k7to'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
